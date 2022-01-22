cdb65dbd2e2b133e78c864c0f501c87b63859ea0
Carlos Vera <kikiondo117@gmail.com>
2022-01-22 12:23:05 -0600

    Nueva funcion c:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// * Utils
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+// * Components
+import { Gallery } from "./components/Gallery";
+import { Form } from "./components/Form";
 
 function App() {
+  // State
+  const [gallery, setGallery] = React.useState([]);
+
+  const getImages = async () => {
+    // Interactuar con nuestra API
+    const resp = await axios.get(`${process.env.REACT_APP_API}photo`);
+
+    if (resp.data.detail) {
+      setGallery(resp.data.detail);
+    }
+    return null;
+  };
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    await axios.post(`${process.env.REACT_APP_API}photo`, formData);
+    getImages();
+  };
+
+  React.useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Home c:</h1>
+        <Routes>
+          <Route path="/gallery" element={<Gallery images={gallery} />} />
+          <Route
+            path="/form"
+            element={<Form handleOnSubmit={handleOnSubmit} />}
+          />
+        </Routes>
       </header>
     </div>
   );
